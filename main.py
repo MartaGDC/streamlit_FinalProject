fd2370bd5ac137ab104adffd6b68c3ba68009958
MartaGDC <gdc.marta@gmail.com>
2023-12-31 01:19:09 +0100

    cambios

--- main.py
+++ main.py
@@ -27,7 +27,10 @@ enlace_url = 'https://www.queeringthemap.com/'
 main_content = f"<h2> The Map </h2> <div class='centered-link'><a href='{enlace_url}'>www.queeringthemap.com</a></div><h1>  </h1>"
 st.markdown(main_content, unsafe_allow_html=True) # The web cannot be embeded in streamlit because of security issues
 
-explanation_content = f"In 2017, the webpage Queeringthemap.com was created for sharing stories and experiences lived by queer people in order to protect and keep their voices. This page is for presenting the result of my final project in Ironhack Data Analysis Bootcamp: the analysis of the discourse in this page.<h1>  </h1>"
+explanation_content = """In 2017, the webpage Queeringthemap.com was created for sharing stories and experiences lived by queer people in order to protect and keep their voices. 
+The information of the page arises this question: \n
+'*How does race, gender, sexuality, citizenship, ability, and class affect the ways in which we relate to, move through, and create space?*' \n
+This page shows some of the results of my final project in Ironhack Data Analysis Bootcamp, trying to give some insights in the process of answering this question, respecting the anonimity of the community involved, thorugh the analysis of the discourse of the shared experiences and it's relationship with the physical space defined by each country."""
 st.markdown(explanation_content, unsafe_allow_html=True)
  
 map_image = "images/map_frontpage.png"
