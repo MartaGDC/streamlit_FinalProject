545be575594d9fdfe6ad140cb90f175c20539ef3
MartaGDC <gdc.marta@gmail.com>
2024-01-03 14:07:52 +0100

    nltk

--- src/words_queries.py
+++ src/words_queries.py
@@ -4,6 +4,7 @@ import pandas as pd
 import nltk
 nltk.download('wordnet')
 nltk.download('vader_lexicon')
+nltk.download("stopwords")
 from nltk.corpus import stopwords
 # import pymysql
 # import sqlalchemy as alch
